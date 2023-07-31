92e7021c9275ff4e84c2c98c29f45dc3d6a66836
fares-ubidata <131752202+fares-ubidata@users.noreply.github.com>
2023-07-31 14:27:00 +0200

    update ci cd

--- ci_cd_script.dart
+++ ci_cd_script.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  final command = 'melos bootstrap';
+  final command = 'melos ls';
 
   // final result = Process.runSync('sh', ['-c', command], runInShell: true);
   // final result = Process.runSync('cmd.exe', ['/c', command], runInShell: true);
